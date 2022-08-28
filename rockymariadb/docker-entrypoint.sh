3f129fc40529871288f03142f96af3f26ef18e8b
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-28 22:10:45 +0200

    Fixed user bug in rockymariadb

--- rockymariadb/docker-entrypoint.sh
+++ rockymariadb/docker-entrypoint.sh
@@ -460,12 +460,6 @@ _main() {
 		docker_setup_env "$@"
 		docker_create_db_directories
 
-		# If container is started as root user, restart as dedicated mysql user
-		if [ "$(id -u)" = "0" ]; then
-			mysql_note "Switching to dedicated user 'mysql'"
-			exec gosu mysql "${BASH_SOURCE[0]}" "$@"
-		fi
-
 		# there's no database, so it needs to be initialized
 		if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
 			docker_verify_minimum_env
