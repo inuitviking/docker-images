e488cdf58178078fbef2644713aa6a821a5d59cf
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-30 12:54:44 +0200

    asdf

--- lpos-mqtt-client/mqtt-client.php
+++ lpos-mqtt-client/mqtt-client.php
@@ -86,9 +86,9 @@ function insertBPM ($db, string $topic, string $message): void {
 
 	print_r($topic);
 
-	$select = sprintf("SELECT bed FROM bpm WHERE bed = %s", $topic[3]);
-	$insert = sprintf("INSERT INTO bpm (bed, bpm) VALUES (%s, %s)", $topic[3], $message);
-	$update = sprintf("UPDATE bpm SET bpm=%s WHERE bed = %s", $message, $topic[3]);
+	$select = sprintf("SELECT bed FROM bpm WHERE bed = '%s'", $topic[3]);
+	$insert = sprintf("INSERT INTO bpm (bed, bpm) VALUES ('%s', '%s')", $topic[3], $message);
+	$update = sprintf("UPDATE bpm SET bpm='%s' WHERE bed = '%s'", $message, $topic[3]);
 
 	echo $select."\n";
 	echo $insert."\n";
