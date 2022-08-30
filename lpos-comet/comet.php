12b51523227225ead5e357799a9acd43d0311319
Angutivik Casper Rúnur Tausen <runurth@gmail.com>
2022-08-30 23:40:40 +0200

    We're now reading from the database

--- lpos-comet/comet.php
+++ lpos-comet/comet.php
@@ -1,9 +1,12 @@
 <?php
 // Be sure we can have all the necessary libraries required.
 require_once 'vendor/autoload.php';
-// Require
+// Require custom classes
 require_once 'classes/Database.php';
 require_once 'classes/CRUD.php';
+// *************
+// * VARIABLES *
+// *************
 /*
  * Servers:
  * - Skúlaheim
@@ -20,42 +23,27 @@ require_once 'classes/CRUD.php';
 $dbserver	= '192.168.80.12';
 $dbuser		= 'Ahmoo';
 $dbpass		= '?&1Q%R>y[lHp,W6KABZy?%l)v#_^';
-$db		= 'infoscreen';
+$db			= 'infoscreen';
 
-$path			= ltrim($_SERVER['REQUEST_URI'],'/');
-$urlParams		= ltrim(substr($path, strpos($path, '?')), '?');
-$urlParams		= explode('&', $urlParams);
-if (str_contains($path, "?")) {
-	$path = substr($path, 0, strpos($path, "?"));
-}
-$elements	= preg_split('/ [\/|?&] /', $path);
+$database = new Database($db, $dbuser, $dbpass, $dbserver);
+$bpmCrud = new Crud($database, 'bpm');
 
-// Define the app
+// ***********
+// * PROGRAM *
+// ***********
+// App settings
 $app = new Comet\Comet([
 	'host' => '0.0.0.0',
 	'port' => 8080,
 ]);
 
-// Setup routing for /bpm
-$app->get($path,
-	function ($request, $response) {
-		$file = 'mqtt.csv';
-		$f = fopen($file, 'r');
-		$data = null;
-		while (($row = fgetcsv($f)) !== false) {
-			$data[] = $row;
-		}
-		fclose($f);
+// What to do on /bpm
+$app->get('/bpm',
+	function ($request, $response) use ($bpmCrud) {
+		$data = $bpmCrud->Read(['*']);
 		return $response
 			->with($data);
 	});
 
-// Start the app
+// Start the service
 $app->run();
-
-
-function GetData (string $path) {
-	$data = null;
-	
-	return $data;
-}
